eaa5446046db70bafb3f7604b342929b93e9c02f
notdoods <khchao@uci.edu>
2020-03-13 15:13:19 -0700

    Create searchExercise.java

---
+++ FitnessAdvisor/app/src/main/java/com/example/fitnessadvisor/searchExercise.java
@@ -0,0 +1,23 @@
+package com.example.fitnessadvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class searchExercise extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+}
